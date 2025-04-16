e83560fdbe404a808e5b0e17dd03de7e3fb63b82
Maryamessanhaji <essanhajimaryam10@gmail.com>
2025-04-16 13:14:35 +0100

    create function update

--- script.js
+++ script.js
@@ -7,38 +7,40 @@ function postStudent() {
     notte: document.getElementById("notte").value.trim(),
     status: "",
   };
-  if (obj.notte ==="" || obj.fullName ==='' || obj.groupy ==="" )  {
-    alert('You must enter all required fields ')
-    return;
-    
-  }
+
   if (obj.notte >= 10 && obj.notte <= 12) {
     obj.status = " PASS";
-   
-  } else if (obj.notte > 12 &&  obj.notte <=14) {
+  } else if (obj.notte > 12 && obj.notte <= 14) {
     obj.status = "Good";
   } else if (obj.notte > 14 && obj.notte <= 16) {
     obj.status = "Average";
   } else if (obj.notte > 16 && obj.notte <= 18) {
     obj.status = "Very Good";
-  } else if (obj.notte >= 18 && obj.notte <=20) {
+  } else if (obj.notte >= 18 && obj.notte <= 20) {
     obj.status = "Excellent ";
-  } else  if (obj.notte >= 0  && obj.notte <= 10) {
+  } else if (obj.notte >= 0 && obj.notte <= 10) {
     obj.status = " Fail";
   }
-  else{
+  else {
     obj.status = "Invalid note entered"
   }
-   
-  db_students.push(obj);
+  if (obj.notte === "" || obj.fullName === '' || obj.groupy === "") {
+    alert('You must enter all required fields ');
+  }
+  else {
+    db_students.push(obj);
+  }
   getStudents();
+
+  document.getElementById("fullName").value = ''
+  document.getElementById("group").value = ''
+  document.getElementById("notte").value = ''
 }
 
 function getStudents() {
   let i;
   let tableData = document.getElementById("tableData");
   tableData.innerHTML = "";
- 
 
   for (i = 0; i < db_students.length; i++) {
     let newTr = document.createElement("tr");
@@ -47,7 +49,7 @@ function getStudents() {
       <td>${db_students[i].groupy}</td>
       <td>${db_students[i].status}</td>
        <td>
-        <button style='  background-color: red;
+        <button onclick='updateStudent(${i})' style='  background-color: red;
         color: white;
     padding: 14px 20px;
     margin: 8px 0;
@@ -60,9 +62,23 @@ function getStudents() {
     margin: 8px 0;
     border: none;
     border-radius: 4px;
-    cursor: pointer;'>delete</button>
+    cursor: pointer;' onclick='deleteStudent(${i})'>delete</button>
       </td>
     `;
     tableData.appendChild(newTr);
   }
+}
+function updateStudent(index) {
+  document.getElementById("fullName").value = db_students[index].fullName
+  document.getElementById("group").value = db_students[index].groupy
+  document.getElementById("notte").value = db_students[index].notte
+  if(newFullName){
+    db_students[index].fullName = newFullName
+
+  }
+getElementById()
+
+}
+function deleteStudent(index){
+
 }
